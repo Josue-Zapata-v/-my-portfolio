9242be51f0001828df436cce7f729a8fbe4b4b01
Josue-Zapata-v <zjosue775@gmail.com>
2026-06-16 01:29:22 -0500

    Actualizar siteUrl al nuevo dominio my-portfolio-josuezv.vercel.app

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -39,6 +39,6 @@ export const personalInfo = {
   email: 'zjosue775@gmail.com',
   github: 'https://github.com/Josue-Zapata-v',
   linkedin: 'https://linkedin.com/in/tuusuario',
-  siteUrl: 'https://my-portfolio-lime-sigma-90.vercel.app',
+  siteUrl: 'https://my-portfolio-josuezv.vercel.app',
   avatar: 'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 };
